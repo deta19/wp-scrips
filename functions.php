a921d16d6f26065eaca4f1689a91968773c5a212
deta15 <deta.mihai@gmail.com>
2020-09-08 22:08:59 +0300

    change from email programatically

--- functions.php
+++ functions.php
@@ -338,3 +338,18 @@ function blogarticle_schemaorg() {
 
 }
 add_action('wp_footer', 'blogarticle_schemaorg');
+
+// Function to change email address
+//more info: https://www.wpbeginner.com/plugins/how-to-change-sender-name-in-outgoing-wordpress-email/
+function wpb_sender_email( $original_email_address ) {
+    return 'tim.smith@example.com';
+}
+ 
+// Function to change sender name
+function wpb_sender_name( $original_email_from ) {
+    return 'Tim Smith';
+}
+ 
+// Hooking up our functions to WordPress filters 
+add_filter( 'wp_mail_from', 'wpb_sender_email' );
+add_filter( 'wp_mail_from_name', 'wpb_sender_name' );
